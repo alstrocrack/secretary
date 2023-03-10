572db84333610758a02590986740db0a46c95bb9
YutoUrushima <yuto259233@gmail.com>
2023-03-10 23:06:46 +0900

    Add some condition

--- ruby.sh
+++ ruby.sh
@@ -4,7 +4,7 @@ ruby() {
   case $1 in
     for) echo "for num in {int}..{int} do ... end"
     ;;
-    if) echo "if ... elsif ...else ... end"
+    if | else | elsif) echo "if ... elsif ...else ... end"
     ;;
     case) echo "case {var} when {var} then ... else ... end"
     ;;
