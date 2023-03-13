becd5a7c50d2b6a9b4e9009e6f828f5f3efe735f
YutoUrushima <yuto259233@gmail.com>
2023-03-13 23:18:46 +0900

    Fix script

--- secretary.sh
+++ secretary.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# source ./ruby.sh
-# source ./javascript.sh
 
-# case $1 in
-#   ruby) ruby $2
-#   ;;
-#   javascript) javascript $2
-#   ;;
-# esac
-
-syntax=$(cat syntax.json | jq ".ruby" | jq ".if")
+# getopts
+syntax=$(cat syntax.json | jq ".$1" | jq ".$2")
 echo $syntax
