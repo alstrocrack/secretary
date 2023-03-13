13571a3adb656f1530c9525aba02c005e0ac0004
YutoUrushima <yuto259233@gmail.com>
2023-03-13 22:50:05 +0900

    Fix comprehensive logic

--- secretary.sh
+++ secretary.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-source ./ruby.sh
+# source ./ruby.sh
+# source ./javascript.sh
 
-ruby $2
+# case $1 in
+#   ruby) ruby $2
+#   ;;
+#   javascript) javascript $2
+#   ;;
+# esac
+
+syntax=$(cat syntax.json | jq ".ruby" | jq ".if")
+echo $syntax
