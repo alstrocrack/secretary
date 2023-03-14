ba89fc5ca59f4f8c426e932029170939b9675951
YutoUrushima <yuto259233@gmail.com>
2023-03-14 20:50:23 +0900

    Arrange env

--- secretary.sh
+++ secretary.sh
@@ -4,20 +4,27 @@ error="Enter valid option"
 
 # language option
 case $1 in
-  -r | --ruby) language=ruby
-  ;;
-  -j | --javascript) language=javascript
-  ;;
-  *) echo $error; exit 1
-  ;;
+  -r | --ruby)
+    language=ruby
+    ;;
+  -j | --javascript)
+    language=javascript
+    ;;
+  *)
+    echo $error
+    exit 1
+    ;;
 esac
 
 # syntax to search
 case $2 in
-  if | else | elsif) syntax=if
-  ;;
-  *) echo $error; exit 1
-  ;;
+  if | else | elsif)
+    syntax=if
+    ;;
+  *)
+    echo $error
+    exit 1
+    ;;
 esac
 
 echo $(cat syntax.json | jq ".$language" | jq ".$syntax")
