75a642a6437646b48978a5bc67570b6bb0df3d38
YutoUrushima <yuto259233@gmail.com>
2023-03-14 20:42:06 +0900

    Fix business logic

--- secretary.sh
+++ secretary.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+error="Enter valid option"
 
-# getopts
-syntax=$(cat syntax.json | jq ".$1" | jq ".$2")
-echo $syntax
+# language option
+case $1 in
+  -r | --ruby) language=ruby
+  ;;
+  -j | --javascript) language=javascript
+  ;;
+  *) echo $error; exit 1
+  ;;
+esac
+
+# syntax to search
+case $2 in
+  if | else | elsif) syntax=if
+  ;;
+  *) echo $error; exit 1
+  ;;
+esac
+
+echo $(cat syntax.json | jq ".$language" | jq ".$syntax")
